bc8942d2a6160e4fe2df66359a91f27c6c1964f4
Oleksii Milevskyi <123121237+omilevskyi@users.noreply.github.com>
2026-05-06 15:40:14 +0300

    chore: Fix auxiliary/port_version.sh (#31)

--- auxiliary/port_version.sh
+++ auxiliary/port_version.sh
@@ -9,15 +9,16 @@ readonly LESS_BIN=/usr/bin/less
 readonly MAKE_BIN=/usr/bin/make
 readonly SED_BIN=/usr/bin/sed
 readonly SORT_BIN=/usr/bin/sort
+
 readonly LOCALBASE=/usr/local
+readonly PORTSINDEXUP_BIN="${LOCALBASE}/bin/portsindexup"
+readonly PKG_BIN="${LOCALBASE}/sbin/pkg-static"
 
 PORTSDIR=$(${MAKE_BIN} -C / -V PORTSDIR)
-# INDEXFILE=$(${MAKE_BIN} -C "${PORTSDIR}" -V INDEXFILE)
 readonly GIT_CMD="${LOCALBASE}/bin/git --no-pager -C ${PORTSDIR}"
 
 if [ -n "${DEBUG}" ]; then
 	echo "PORTSDIR=${PORTSDIR}"
-	# echo "INDEXFILE=${INDEXFILE}"
 	echo "GIT_CMD=${GIT_CMD}"
 fi
 
@@ -32,9 +33,9 @@ fi
 exec ${GIT_CMD} diff --no-color --name-only "${COMMIT_ID}" |
 	${SED_BIN} -rn 's,(.*/.*)/[^/]+$,\1,p' |
 	${SORT_BIN} --unique |
-	${HOME}/bin/portsindexup ${DEBUG:+-verbose} &&
+	${PORTSINDEXUP_BIN} ${DEBUG:+-verbose} &&
 	echo &&
-	${LOCALBASE}/sbin/pkg-static version --verbose --not-like = |
-	${GREP_BIN} --invert-match --regexp '[[:space:]]>[[:space:]]' --regexp '[[:space:]]orphaned:[[:space:]]local/' |
+	${PKG_BIN} version --verbose --not-like = |
+	${GREP_BIN} --invert-match --regexp '[[:blank:]]>[[:blank:]]' --regexp '[[:blank:]]orphaned:[[:blank:]]local/' |
 	${LESS_BIN} --no-init --quit-if-one-screen &&
 	echo
